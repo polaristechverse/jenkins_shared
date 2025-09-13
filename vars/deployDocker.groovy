77b4bc7127104291a2f395079059922ac8b77ea2
polaristechverse <polaristechverse@gmail.com>
2025-09-13 12:50:04 +0530

    Update deployDocker.groovy

--- vars/deployDocker.groovy
+++ vars/deployDocker.groovy
@@ -1,9 +1,9 @@
 def call(String imageName, String containerName) {
     stage('Deploy Docker Container') {
-        echo "Deploying Docker container: ${env.containerName}"
+        echo "Deploying Docker container: ${containerName}"
         // Stop old container if exists
-        sh "docker rm -f ${env.containerName} || true"
+        sh "docker rm -f ${containerName} || true"
         // Run new container
-        sh "docker run -itd --name ${env.containerName} -p 5600:5600 ${env.imageName}"
+        sh "docker run -itd --name ${containerName} -p 5600:5600 ${imageName}"
     }
 }
